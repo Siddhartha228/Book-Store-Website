9142912002f86a73a6517f206d97e952aa8293ee
Siddhartha228 <152017010+Siddhartha228@users.noreply.github.com>
2025-02-18 23:37:37 +0545

    updated login and signup

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -21,7 +21,7 @@ function AdminLogin() {
             <label className="block text-gray-600 mb-1">Password</label>
             <input
               type="password"
-              placeholder="********"
+              placeholder="Enter your password"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
             />
           </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -29,7 +29,15 @@ const Signup = () => {
             <label className="block text-gray-600 mb-1">Password</label>
             <input
               type="password"
-              placeholder="********"
+              placeholder="Create a password"
+              className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
+            />
+          </div>
+          <div>
+            <label className="block text-gray-600 mb-1">Confirm Password</label>
+            <input
+              type="password"
+              placeholder="Re-enter your password"
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-400"
             />
           </div>
